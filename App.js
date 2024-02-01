cc56e68ef6599579a787cf330462fa18029b4246
stas-di55ik <stas.hanchev@gmail.com>
2024-02-01 03:14:13 +0200

    finished lab1

--- App.js
+++ App.js
@@ -1,13 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  state = {
+    coinResult: 'head'
+  }
+  
+  flipACoin() {
+    const random = Math.floor(Math.random() * 2) + 1;
+    if (random === 1) {
+      this.setState({
+        coinResult: 'head'
+      });
+    } else {
+      this.setState({
+        coinResult: 'tail'
+      });
+    }
+  }
+  
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>
+          {this.state.coinResult === 'head' ? 'Head' : 'Tail'}
+        </Text>
+
+        { this.state.coinResult === 'head' ?
+        <Image 
+          source={require('./assets/coin-h.png')}
+          style={styles.image}/> :
+        <Image 
+          source={require('./assets/coin-t.png')}
+          style={styles.image}
+        />}
+        
+        <TouchableOpacity onPress={() => {
+          this.flipACoin();
+        }} style={styles.button}> 
+          <Text>Flip a coin</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +52,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    backgroundColor: '#f1c40f',
+    padding: 20,
+    margin: 20,
+    borderRadius: 4,
+  },
+  image: {
+      width: 150,
+      height: 150
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  }
 });
